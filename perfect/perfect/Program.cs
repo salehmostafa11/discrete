8a2321e10ca5f3eb874c17322725fab8813ab309
salehmostafa11 <120852885+salehmostafa11@users.noreply.github.com>
2022-12-18 12:51:22 +0200

    Add files via upload

---
+++ perfect/perfect/Program.cs
@@ -0,0 +1,25 @@
+﻿int firstNumber, secondNumber, n, i, sum;
+
+Console.WriteLine("Enter the first number");
+firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+secondNumber = int.Parse(Console.ReadLine());
+
+for ( n = firstNumber; n <= secondNumber; n++)
+{
+	 i = 1;
+	sum = 0;
+	while (i < n)
+	{
+        if (n % i == 0)
+            sum = sum + i;
+        i++;
+    }
+	if (sum == n && n !=0)
+	{
+		Console.WriteLine("{0} ", n);
+	}
+	
+		
+}
---
+++ prime/prime/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstNumber, secondNumber, sum = 0;
+
+Console.WriteLine("Please enter the first number");
+firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number");
+secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n The prime numbers between {0} and {1} is : \n", firstNumber, secondNumber);
+
+
+for (int i = firstNumber; i <= secondNumber; i++)
+{
+    sum = 0;
+    for (int k = 2; k <= i / 2; k++)
+    {
+        if (i % k == 0)
+        {
+            sum = sum + 1;
+            break;
+        }
+    }
+    if (sum == 0 && i != 1 && i != 0)
+    {
+        Console.WriteLine("\t {0}", i);
+    }
+}
